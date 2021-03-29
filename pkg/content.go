19e961bc8d51ad113f05633068af82037305626f
saman <saman.hoseini@snapp.cab>
2021-03-29 15:33:55 +0430

    feat: add pkg shared files

---
+++ pkg/content.go
@@ -0,0 +1,8 @@
+package pkg
+
+var (
+	ContentType = "Content-Type"
+	FileType    = "application/octet-stream"
+	FormType    = "application/x-www-from-urlencoded"
+	JsonType    = "application/jsonType"
+)
---
+++ pkg/help.go
@@ -0,0 +1,11 @@
+package pkg
+
+const (
+	MethodUsage  = "To determine request method"
+	HeaderUsage  = "To determine request headers"
+	QueriesUsage = "To determine request query string"
+	DataUsage    = "To determine request form data"
+	JsonUsage    = "To input data in json format"
+	FileUsage    = "To send a file"
+	TimeoutUsage = "To determine request timeout"
+)
